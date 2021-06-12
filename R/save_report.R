f9a5b7d6d1052dfe6f6c9c5d7191a099958d5ab2
John Harrold <john.m.harrold@gmail.com>
2021-06-12 05:41:44 -0700

    Couple other things I missed as well.

---
+++ R/save_report.R
@@ -0,0 +1,77 @@
+#'@export
+#'@title Save Onbrand Report to a File
+#'@description Saves report in onbrand object to the specified file.
+#'
+#'@param obnd onbrand report object
+#'@param output_file name of file to place the annotated layout information, set to \code{NULL} and it will generate a file named layout with the appropriate extension
+#'@param verbose Boolean variable when set to TRUE (default) messages will be
+#'displayed on the terminal; Messages will be included in the returned onbrand
+#'object.
+#'
+#'@return List with the following elements
+#' \itemize{
+#' \item{isgood} Boolean variable indicating success or failure
+#' \item{rpt} Officer with the annotated layout
+#' \item{msgs} Vector of messages
+#'}
+#'@examples
+#'
+#'obnd = read_template(
+#'       template = file.path(system.file(package="onbrand"), "templates", "report.pptx"),
+#'       mapping  = file.path(system.file(package="onbrand"), "templates", "report.yaml"))
+#'
+#' save_report(obnd, tempfile(fileext = ".pptx"))
+#'
+#'
+save_report  = function (obnd,
+                         output_file = NULL,
+                         verbose     = TRUE){
+
+  msgs          = c()
+  isgood        = TRUE
+  rpttype       = "Unknown"
+
+
+  if(obnd[["isgood"]]){
+     rpttype = obnd[["rpttype"]]
+
+  } else {
+    isgood = FALSE
+    msgs = c(msgs, "Bad onbrand object supplied")
+  }
+
+  if(is.null(output_file)){
+    isgood = FALSE
+    msgs   = c(msgs, "You need to specify an output_file name.")
+  } else {
+    fr = fetch_rpttype(output_file, verbose=verbose)
+    # Appending any messages from fetch_rpttype if verbose was turned off
+    if(!fr[["isgood"]] & !verbose){
+      msgs = c(msgs, fr[["msgs"]])
+    }
+    # Making sure the file extensions match
+    if(fr[["rptext"]] != obnd[["rptext"]]){
+      isgood = FALSE
+      msgs = c(msgs, paste0("The output_file type >", fr[["rpttype"]], "< dose not match the report " ))
+      msgs = c(msgs, paste0("found in the supplied onbrand object >", obnd[["rpttype"]], "<." ))
+      msgs = c(msgs, paste0("You must supply an output file with a >.", obnd[["rptext"]], "< file extension." ))
+    }
+  }
+
+  # If we pass all the checks above we save the file
+  if(isgood){
+    print(obnd[["rpt"]], output_file)
+  }
+
+  # If errors were encountered we make sure that the state of the reporting
+  # object is set to false
+  if(!isgood){
+    obnd[["isgood"]] = FALSE
+    msgs = c(msgs, "save_report()")
+  }
+
+  # Dumping the messages if verbose is turned on:
+  if(verbose & !is.null(msgs)){
+    message(paste(msgs, collapse="\n"))
+  }
+}
