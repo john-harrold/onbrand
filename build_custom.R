c7cd6f2b44b02913b1e0311590eedb5df92c604b
John Harrold <john.m.harrold@gmail.com>
2021-07-30 21:05:44 -0700

    Rearranging figure locations and references.

---
+++ build_custom.R
@@ -0,0 +1,24 @@
+# Rebuilding the pkgdown site
+pkgdown::build_site()
+
+# Fixing any broken image references
+art_dir = file.path("docs", "articles")
+
+# Getting all of the html files in the article dir
+htds = dir(art_dir, "*.html")
+
+for(htd in htds){
+  fn = file.path(art_dir, htd)
+
+  cfn = file(fn, open="r")
+  htd_lines = readLines(cfn)
+  close(cfn)
+
+  # For some reason it's doing this weird relative path thing, so I'm stripping that out here:
+  #trim_txt = "../../../../../Google%20Drive/Ubiquity/github/ubiquity/vignettes/"
+  trim_txt = "../../../../../Google%20Drive/onbrand/github/onbrand/vignettes/"
+  htd_lines = gsub(trim_txt, "", htd_lines)
+
+  write(htd_lines, file=fn, append=FALSE)
+
+}
